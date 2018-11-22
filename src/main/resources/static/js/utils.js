3a93d9961f4da08a9fd8155513f2b9a58642f9d9
analimazn <analimazn@gmail.com>
2018-11-22 00:38:56 -0200

    Mudança no layout do app

--- src/main/resources/static/js/utils.js
+++ src/main/resources/static/js/utils.js
@@ -2,29 +2,32 @@ $(document).ready(function(){
 			
 
 			$("#busca").click(function() {
-
+				
+				/*  FIXME:
+				 *	Precisa achar algum jeito de fazer a tabela nao aparecer se a pessoa clicou no botão
+				 *	sem escolher um genero. 
+				 */
+				//document.getElementById('tabela').style.display = "block";
+				/*  FIXME:
+				 *	Isso é uma gambiarra pra o bg preto nao 'acabar' quando a tabela aparecer
+				 */
+				//document.getElementById('content').style.height = "150vh";
 				var genero = $( "#genero option:selected" ).text();
 				
 				$("#dadosFilme1").empty();
 				$("#dadosFilme2").empty();
 				$("#dadosFilme3").empty();
-
-				$("#dadosFilme").empty();
 					
 				var url = "/filme/"+genero+"";
 				
 				$.getJSON(url, function(data) {
 						
 						var itensDivs = [];
-						var itensTable = [];
 						
 						$.each(data, function(i){
 						
 							itensDivs.push("<p>" + this.nome + "</p><p>" + this.diretor + "</p><p>" + 
-								this.genero + "</p><p>"+ this.ano + "</p><p>");
-
-							itensTable.push("<tr><td>" + this.nome + "</td><td>" + this.diretor + 
-								"</td><td>" + this.genero + "</td><td>" + this.ano + "</td></tr>");
+								this.genero + "</p><p>"+ this.ano);
 
 						});
 
@@ -34,12 +37,9 @@ $(document).ready(function(){
 						var indexTwo = Math.floor(Math.random() * 5);
 						var indexThree = Math.floor(Math.random() * 5);
 
-						console.log(indexOne + " " + indexTwo + " " + indexThree);
-
 						$("#dadosFilme1").append(itensDivs[indexOne]);
 						$("#dadosFilme2").append(itensDivs[indexTwo]);
 						$("#dadosFilme3").append(itensDivs[indexThree]);
-						$("#dadosFilme").append(itensTable);
 						
 				});
 
